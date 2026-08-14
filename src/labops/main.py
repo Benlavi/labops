ac8a762a5c11ac153f038b98560070cece82b1c2
Ben Lavi <87126461+Benlavi@users.noreply.github.com>
2026-08-14 12:27:11 +0300

    add cli and disk usage information

--- src/labops/main.py
+++ src/labops/main.py
@@ -8,7 +8,11 @@ from datetime import timedelta
 parser = argparse.ArgumentParser(description="LabOps Host Inspector: Displays system information.")
 subparsers = parser.add_subparsers(dest="command")
 
-parser_info = subparsers.add_parser("info" , help="Display system information.")
+subparsers.add_parser("info" , help="Display system information.")
+subparsers.add_parser("disk" , help="Display disk usage information.")
+
+################################################################################################################
+#info 
 
 def get_hostname() -> str:
     return socket.gethostname()
@@ -30,13 +34,35 @@ def show_system_info() -> None:
     print(f"Kernel Version: {get_kernel_version()}")  
     print(f"Uptime: {get_uptime()}")
 
+################################################################################################################
+#disk
+def get_disk_usage() -> dict[str, str]:
+    disk_usage = psutil.disk_usage('/')
+    return {
+        "total": f"{disk_usage.total / (1000 ** 3):.2f} GB",
+        "used": f"{disk_usage.used / (1000 ** 3):.2f} GB",
+        "free": f"{disk_usage.free / (1000 ** 3):.2f} GB",
+        "percent": f"{disk_usage.percent}%"
+    }
+  
 
+def show_disk_info() -> None:
+    disk_usage_info = get_disk_usage()
+    print("=== Disk Information ===")
+    print(f"Total Disk Space: {disk_usage_info['total']}")
+    print(f"Used Disk Space: {disk_usage_info['used']}")
+    print(f"Free Disk Space: {disk_usage_info['free']}")
+    print(f"Disk Usage Percentage: {disk_usage_info['percent']}")
 
 def main() -> None:
     args = parser.parse_args()
     if args.command == "info":
         show_system_info()
+    elif args.command == "disk":
+        show_disk_info()
     else:
         parser.print_help()
+
+
 if __name__ == "__main__":
     main()
