b5d396d460af380fff1b99b45e37e3e86b982f32
Ben Lavi <87126461+Benlavi@users.noreply.github.com>
2026-08-17 00:09:51 +0300

    Add log capture test

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,6 @@
-from labops import main , health
 import sys
+from labops import main, health
+
 
 def test_get_exit_status():
     assert main.get_exit_status(None) == 0
@@ -9,13 +10,23 @@ def test_get_exit_status():
     assert main.get_exit_status("Banana") == 3
 
 
-def test_main_return_error_code_when_health_fails(monkeypatch):
+def test_main_return_error_code_when_health_fails(monkeypatch,caplog):
     mock_args = ['labops','health']
     monkeypatch.setattr(sys,"argv",mock_args)
+
     def mock_system_health():
         raise RuntimeError("Failed to get data")
-    monkeypatch.setattr(health,"get_system_health",mock_system_health)
+    
+    monkeypatch.setattr(
+        health,
+        "get_system_health",
+        mock_system_health
+        )
+    
     return_value = main.main()
+    
     assert return_value == 3
+    assert "Failed to collect system health" in caplog.text
+    assert "Failed to get data" in caplog.text
 
    
