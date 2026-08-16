d4e57982e3f108cf4a278f2d8457f197381c2d83
Ben Lavi <87126461+Benlavi@users.noreply.github.com>
2026-08-17 00:02:04 +0300

    add catch error test

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,5 @@
-from labops import main
+from labops import main , health
+import sys
 
 def test_get_exit_status():
     assert main.get_exit_status(None) == 0
@@ -6,3 +7,15 @@ def test_get_exit_status():
     assert main.get_exit_status("WARNING") == 1
     assert main.get_exit_status("CRITICAL") == 2
     assert main.get_exit_status("Banana") == 3
+
+
+def test_main_return_error_code_when_health_fails(monkeypatch):
+    mock_args = ['labops','health']
+    monkeypatch.setattr(sys,"argv",mock_args)
+    def mock_system_health():
+        raise RuntimeError("Failed to get data")
+    monkeypatch.setattr(health,"get_system_health",mock_system_health)
+    return_value = main.main()
+    assert return_value == 3
+
+   
