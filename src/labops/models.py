38ea4088c412f573ac2aeb03b72d508f0c3bf12a
Ben Lavi <87126461+Benlavi@users.noreply.github.com>
2026-08-18 00:08:41 +0300

    Added pydantic validation

---
+++ src/labops/models.py
@@ -0,0 +1,34 @@
+from pydantic import BaseModel
+
+class SystemInfo(BaseModel):
+
+    hostname: str
+    os_name: str
+    kernel_version: str
+    uptime_seconds: float
+
+class HealthInfo(BaseModel):
+    memory_percent: float
+    disk_percent: float
+    memory_status: str
+    disk_status: str
+    overall_status: str
+
+class DiskInfo(BaseModel):
+    total_gb: float 
+    used_gb: float 
+    free_gb: float 
+    percent: float
+
+class NetworkInterfaceInfo(BaseModel):
+    IPv4: str | None = None
+    IPv6: str |  None = None
+    MAC: str |  None = None
+    status: str | None = None
+
+
+class Report(BaseModel):
+    system: SystemInfo
+    disk: DiskInfo
+    network: dict[str, NetworkInterfaceInfo]
+    health: HealthInfo
--- src/labops/server.py
+++ src/labops/server.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI
+from labops.models import Report
 app = FastAPI()
 
 
 @app.post("/reports")
-def receive_report(report: dict):
+def receive_report(report: Report):
     received={
         "status": "received",
-        "hostname": report['system']['hostname'],
-        "health": report['health']['overall_status']
+        "hostname": report.system.hostname,
+        "health": report.health.overall_status
         }
     return received
