f83d5f34f6d6214427b53e5e665e2794aaddab7f
Ben Lavi <87126461+Benlavi@users.noreply.github.com>
2026-08-17 23:15:08 +0300

    Parse report on server

--- src/labops/server.py
+++ src/labops/server.py
@@ -4,5 +4,9 @@ app = FastAPI()
 
 @app.post("/reports")
 def receive_report(report: dict):
-    received={"status": "received"}
+    received={
+        "status": "received",
+        "hostname": report['system']['hostname'],
+        "health": report['health']['overall_status']
+        }
     return received
